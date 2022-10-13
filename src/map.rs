18d273e979b8a4742ca477ed9c13987c124be33c
petrostrak <pit.trak@gmail.com>
2022-10-14 02:46:26 +0300

    Render the map

--- src/map.rs
+++ src/map.rs
@@ -21,4 +21,20 @@ impl Map {
             tiles: vec![TileType::Floor; NUM_TILES]
         }
     }
+
+    pub fn render(&self, ctx: &mut BTerm) {
+        for y in 0..SCREEN_HEIGHT {
+            for x in 0..SCREEN_WIDTH {
+                let idx = map_idx(x, y);
+                match self.tiles[idx] {
+                    TileType::Floor => {
+                        ctx.set(x, y, YELLOW, BLACK, to_cp437('.'));
+                    }
+                    TileType::Wall => {
+                        ctx.set(x, y, GREEN, BLACK, to_cp437('#'));
+                    }
+                }
+            }
+        }
+    }
 }
