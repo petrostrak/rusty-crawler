597c7046e18894c297ca5bcbd9ecc27792ca7ce9
petrostrak <pit.trak@gmail.com>
2022-10-14 02:42:35 +0300

    Index the map

--- src/map.rs
+++ src/map.rs
@@ -1,2 +1,24 @@
 use crate::prelude::*;
 const NUM_TILES: usize = (SCREEN_WIDTH * SCREEN_HEIGHT) as usize;
+
+#[derive(Copy, Clone, PartialEq)]
+pub enum TileType {
+    Wall,
+    Floor,
+}
+
+pub struct Map {
+    pub tiles: Vec<TileType>,
+}
+
+pub fn map_idx(x: i32, y: i32) -> usize {
+    ((y * SCREEN_WIDTH) + x) as usize
+}
+
+impl Map {
+    pub fn new() -> Self {
+        Self {
+            tiles: vec![TileType::Floor; NUM_TILES]
+        }
+    }
+}
