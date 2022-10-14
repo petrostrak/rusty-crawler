2b52c32db3902367a5a0374e805db42ed9e53dbd
petrostrak <pit.trak@gmail.com>
2022-10-14 03:00:31 +0300

    Add in_bounds, can_enter_tiles and try_idx map methods

--- src/map.rs
+++ src/map.rs
@@ -37,4 +37,29 @@ impl Map {
             }
         }
     }
+
+    // This function checks that the location specified in point 
+    // is greater than zero on both the x and y axes, and that 
+    // it is less than the screen height and width.
+    pub fn in_bounds(&self, point: Point) ->bool {
+        point.x >= 0 && point.x < SCREEN_WIDTH
+            && point.y >= 0 && point.y < SCREEN_HEIGHT
+    }
+
+    // Determine if the player can enter a tile.
+    pub fn can_enter_tile(&self, point: Point) -> bool {
+        self.in_bounds(point)
+            && self.tiles[map_idx(point.x, point.y)] == TileType::Floor
+    }
+
+    // Determine the tile's index coordinates. Indicate an error 
+    // condition if the requested coordinates fall outside of the 
+    // map boundaries.
+    pub fn try_idx(&self, point: Point) -> Option<usize> {
+        if !self.in_bounds(point) {
+            None
+        } else {
+            Some(map_idx(point.x, point.y))
+        }
+    }
 }
