4142b19948639333676e86a41f1ef7692b3b85f6
petrostrak <pit.trak@gmail.com>
2022-10-15 16:26:13 +0300

    Remove player rs

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@
 
 mod map;
 mod map_builder;
-mod player;
 mod camera;
 
 mod prelude {
@@ -15,7 +14,6 @@ mod prelude {
     pub const DISPLAY_WIDTH: i32 = SCREEN_WIDTH / 2; 
     pub const DISPLAY_HEIGHT: i32 = SCREEN_HEIGHT / 2;
     pub use crate::map::*;
-    pub use crate::player::*;
     pub use crate::map_builder::*;
     pub use crate::camera::*;
 }
@@ -24,7 +22,6 @@ use prelude::*;
 
 struct State {
     map: Map,
-    player: Player,
     camera: Camera,
 }
 
@@ -34,7 +31,6 @@ impl State {
         let map_builder = MapBuilder::new(&mut rng);
         Self {
             map : map_builder.map,
-            player: Player::new(map_builder.player_start),
             camera: Camera::new(map_builder.player_start),
         }
     }
@@ -46,7 +42,8 @@ impl GameState for State {
         ctx.cls();
         ctx.set_active_console(1);
         ctx.cls();
-        self.player.update(ctx, &self.map, &mut self.camera); self.map.render(ctx, &self.camera); self.player.render(ctx, &self.camera);
+        // TODO: Execute Systems
+        // TODO: Render Draw Buffer
     }
 }
 
