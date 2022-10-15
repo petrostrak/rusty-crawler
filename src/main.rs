310f81d3193de4fbe14b23ab8876574b1b96c996
petrostrak <pit.trak@gmail.com>
2022-10-15 15:05:34 +0300

    Include player into gamestate

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,16 @@
+#![warn(clippy::pedantic)]
+
 mod map;
 mod map_builder;
 mod player;
 
 mod prelude {
-    pub use bracket_lib::prelude::*; 
-    pub const SCREEN_WIDTH: i32 = 80; 
-    pub const SCREEN_HEIGHT: i32 = 50; 
+    pub use bracket_lib::prelude::*;
+    pub const SCREEN_WIDTH: i32 = 80;
+    pub const SCREEN_HEIGHT: i32 = 50;
     pub use crate::map::*;
-    pub use crate::map_builder::*;
     pub use crate::player::*;
+    pub use crate::map_builder::*;
 }
 
 use prelude::*;
@@ -22,8 +24,8 @@ impl State {
     fn new() -> Self {
         let mut rng = RandomNumberGenerator::new();
         let map_builder = MapBuilder::new(&mut rng);
-        Self { 
-            map: map_builder.map,
+        Self {
+            map : map_builder.map,
             player: Player::new(map_builder.player_start),
         }
     }
@@ -32,12 +34,13 @@ impl State {
 impl GameState for State {
     fn tick(&mut self, ctx: &mut BTerm) {
         ctx.cls();
+        self.player.update(ctx, &self.map);
         self.map.render(ctx);
         self.player.render(ctx);
     }
 }
 
-fn main() -> BError{
+fn main() -> BError {
     let context = BTermBuilder::simple80x50()
         .with_title("Rusty Crawler")
         .with_fps_cap(30.0)
