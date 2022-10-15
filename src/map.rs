19e09bfd66a3f2d41fa8248e675db887a1494a45
petrostrak <pit.trak@gmail.com>
2022-10-15 16:27:39 +0300

    Remove render method from map impl

--- src/map.rs
+++ src/map.rs
@@ -22,37 +22,6 @@ impl Map {
         }
     }
 
-    pub fn render(&self, ctx: &mut BTerm, camera: &Camera) {
-        ctx.set_active_console(0);
-        for y in camera.top_y .. camera.bottom_y {
-            for x in camera.left_x .. camera.right_x {
-                if self.in_bounds(Point::new(x, y)) {
-                    let idx = map_idx(x, y);
-                    match self.tiles[idx] {
-                        TileType::Floor => {
-                            ctx.set(
-                                x - camera.left_x, 
-                                y - camera.top_y, 
-                                WHITE, 
-                                BLACK, 
-                                to_cp437('.')
-                            );
-                        }
-                        TileType::Wall => {
-                            ctx.set(
-                                x - camera.left_x, 
-                                y - camera.top_y, 
-                                WHITE, 
-                                BLACK, 
-                                to_cp437('#')
-                            );
-                        }
-                    }
-                }
-            }
-        }
-    }
-
     // This function checks that the location specified in point 
     // is greater than zero on both the x and y axes, and that 
     // it is less than the screen height and width.
