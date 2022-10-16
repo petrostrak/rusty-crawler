0a94f229dfb6d968852b15a5b2defab07d434b13
petrostrak <pit.trak@gmail.com>
2022-10-16 23:57:11 +0300

    Add monsters and place them to the map

--- src/spawner.rs
+++ src/spawner.rs
@@ -12,3 +12,25 @@ pub fn spawn_player(ecs: &mut World, pos: Point) {
         )
     );
 }
+
+pub fn spawn_monster(
+    ecs: &mut World,
+    rng: &mut RandomNumberGenerator,
+    pos: Point
+) {
+    ecs.push(
+        (
+            Enemy,
+            pos,
+            Render{
+                color: ColorPair::new(WHITE, BLACK),
+                glyph: match rng.range(0,4) {
+                    0 => to_cp437('E'),
+                    1 => to_cp437('0'),
+                    2 => to_cp437('o'),
+                    _ => to_cp437('g'),
+                }
+            }
+        )
+    );
+}
