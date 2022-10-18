fcd45cf7ffbb17a5b82ca89d0c1a3cf3f2ed539e
petrostrak <pit.trak@gmail.com>
2022-10-18 19:47:36 +0300

    Giving Monsters Names and Hit Points

--- src/spawner.rs
+++ src/spawner.rs
@@ -19,20 +19,32 @@ pub fn spawn_monster(
     rng: &mut RandomNumberGenerator,
     pos: Point
 ) {
+
+    // You can destructure data out of tuples using this syntax. 
+    // let (a, b, c) = function_returns_three_values. This lets you name your variables
+    let (hp, name, glyph) = match rng.roll_dice(1, 10) {
+        1..=8 => goblin(),
+        _ => orc(),
+    };
     ecs.push(
         (
             Enemy,
             pos,
             Render{
                 color: ColorPair::new(WHITE, BLACK),
-                glyph: match rng.range(0,4) {
-                    0 => to_cp437('E'),
-                    1 => to_cp437('0'),
-                    2 => to_cp437('o'),
-                    _ => to_cp437('g'),
-                }
+                glyph,
             },
-            MovingRandomly{}
+            MovingRandomly{},
+            Health{current: hp, max: hp},
+            Name(name),
         )
     );
+}
+
+fn goblin() -> (i32, String, FontCharType) {
+    (1, "Goblin".to_string(), to_cp437('g'))
+}
+
+fn orc() -> (i32, String, FontCharType) {
+    (2, "Orc".to_string(), to_cp437('o'))
 }
