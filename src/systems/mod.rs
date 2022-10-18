35ad5fb75c578d3f0aa3a967a74a38681a499b6f
petrostrak <pit.trak@gmail.com>
2022-10-18 20:13:32 +0300

    Remove the Collision System

--- src/systems/mod.rs
+++ src/systems/mod.rs
@@ -1,4 +1,3 @@
-mod collisions;
 mod end_turn;
 mod entity_render;
 mod hud;
@@ -25,8 +24,6 @@ pub fn build_player_scheduler() -> Schedule {
     Schedule::builder()
         .add_system(movement::movement_system())
         .flush()
-        .add_system(collisions::collisions_system())
-        .flush()
         .add_system(map_render::map_render_system())
         .add_system(entity_render::entity_render_system())
         .add_system(end_turn::end_turn_system())
@@ -40,8 +37,6 @@ pub fn build_monster_scheduler() -> Schedule {
         .add_system(random_moves::random_move_system())
         .flush()
         .add_system(movement::movement_system())
-        .flush()
-        .add_system(collisions::collisions_system())
         .flush()
         .add_system(map_render::map_render_system())
         .add_system(entity_render::entity_render_system())
