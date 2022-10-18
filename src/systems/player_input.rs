8b421f9d083c1795fc212b72cfa97dc5faa707c8
petrostrak <pit.trak@gmail.com>
2022-10-18 15:33:05 +0300

    Simplified Player Input

--- src/systems/player_input.rs
+++ src/systems/player_input.rs
@@ -15,11 +15,13 @@ use crate::prelude::*;
 #[read_component(Player)]
 pub fn player_input(
     ecs: &mut SubWorld,
-    #[resource] map: &Map,
+    commands: &mut CommandBuffer,
     #[resource] key: &Option<VirtualKeyCode>,
-    #[resource] camera: &mut Camera,
     #[resource] turn_state: &mut TurnState,
 ) {
+    let mut players = <(Entity, &Point)>::query()
+        .filter(component::<Player>());
+
     if let Some(key) = key {
         let delta = match key {
             VirtualKeyCode::Left => Point::new(-1, 0),
@@ -28,28 +30,13 @@ pub fn player_input(
             VirtualKeyCode::Down => Point::new(0, 1),
             _ => Point::new(0, 0),
         };
-        if delta.x != 0 || delta.y != 0 {
-            
-            // Access components with a query. Queries list one or more components, and return
-            // references (mutable if &mut is used), to each instance of that component type.
-            // If you request more than one component type, only entities with all of those 
-            // components will be grouped together.
-            let mut players = <&mut Point>::query()
-            
-                // You don't want to update all Point components, just the player. Otherwise, when
-                // monsters and items would move when the player moves. Note that the query doesn't
-                // become an iterator until you call iter() or iter_mut(), it's still a query.
-                .filter(component::<Player>());
-            
-            // Calling iter_mut() runs the query and places the results in an iterator.
-            players.iter_mut(ecs).for_each(|pos| {
-                let destination = *pos + delta;
-                if map.can_enter_tile(destination) {
-                    *pos = destination;
-                    camera.on_player_move(destination);
-                    *turn_state = TurnState::PlayerTurn;
-                }
-            })
-        }
+
+        // Calling iter_mut() runs the query and places the results in an iterator.
+        players.iter(ecs).for_each(| (entity, pos) | {
+            let destination = *pos + delta;
+            commands
+                .push(((), WantsToMove{ entity: *entity, destination }));
+        });
+        *turn_state = TurnState::PlayerTurn;
     }
 }
