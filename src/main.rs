a97b9c46d5836e75032b2cb76876e0d371de769c
petrostrak <pit.trak@gmail.com>
2022-10-18 15:46:47 +0300

    Adding Another Rendering Layer

--- src/main.rs
+++ src/main.rs
@@ -67,6 +67,8 @@ impl GameState for State {
         ctx.cls();
         ctx.set_active_console(1);
         ctx.cls();
+        ctx.set_active_console(2);
+        ctx.cls();
 
         // This makes the current keyboard state available 
         // to any system that requests it.
@@ -105,10 +107,13 @@ fn main() -> BError {
         .with_title("Rusty Crawler")
         .with_fps_cap(30.0)
         .with_dimensions(DISPLAY_WIDTH, DISPLAY_HEIGHT)
+        .with_tile_dimensions(32, 32)
         .with_resource_path("resources/")
         .with_font("dungeonfont.png", 32, 32)
+        .with_font("terminal8x8.png", 8, 8)
         .with_simple_console(DISPLAY_WIDTH, DISPLAY_HEIGHT, "dungeonfont.png")
         .with_simple_console_no_bg(DISPLAY_WIDTH, DISPLAY_HEIGHT, "dungeonfont.png")
+        .with_simple_console_no_bg(DISPLAY_WIDTH*8, DISPLAY_HEIGHT*2, "terminal8x8.png")
         .build()?;
 
     main_loop(context, State::new())
