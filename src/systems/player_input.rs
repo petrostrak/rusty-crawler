6342fdcd122d6199571eca27369d26ed3c6d0d04
petrostrak <pit.trak@gmail.com>
2022-10-19 17:53:45 +0300

    Waiting as a strategy

--- src/systems/player_input.rs
+++ src/systems/player_input.rs
@@ -14,8 +14,9 @@ use crate::prelude::*;
 #[read_component(Point)]
 #[read_component(Player)]
 #[read_component(Enemy)]
+#[read_component(Health)]
 pub fn player_input(
-    ecs: &SubWorld,
+    ecs: &mut SubWorld,
     commands: &mut CommandBuffer,
     #[resource] key : &Option<VirtualKeyCode>,
     #[resource] turn_state : &mut TurnState
@@ -36,6 +37,7 @@ pub fn player_input(
                 .unwrap();
 
         let mut enemies = <(Entity, &Point)>::query().filter(component::<Enemy>());
+        let mut did_something = false;
         if delta.x !=0 || delta.y != 0 {
             let mut hit_something = false;
             enemies
@@ -45,6 +47,7 @@ pub fn player_input(
                 })
                 .for_each(|(entity, _) | {
                     hit_something = true;
+                    did_something = true;
 
                     commands
                         .push(((), WantsToAttack{
@@ -54,11 +57,22 @@ pub fn player_input(
                 });
 
             if !hit_something {
+                did_something = true;
                 commands
                     .push(((), WantsToMove{
                         entity: player_entity,
                         destination
                     }));
+            }
+
+            if !did_something {
+                if let Ok(mut health) = ecs
+                    .entry_mut(player_entity)
+                    .unwrap()
+                    .get_component_mut::<Health>()
+                {
+                    health.current = i32::min(health.max, health.current+1);
+                }
             }
         }
         *turn_state = TurnState::PlayerTurn;
