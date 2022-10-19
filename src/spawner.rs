c285f7b899aaeb93d40cac1684a71b84a30f83b4
petrostrak <pit.trak@gmail.com>
2022-10-19 18:29:12 +0300

    Heat-seeking monsters

--- src/spawner.rs
+++ src/spawner.rs
@@ -9,7 +9,7 @@ pub fn spawn_player(ecs: &mut World, pos: Point) {
                 color: ColorPair::new(WHITE, BLACK),
                 glyph: to_cp437('@')
             },
-            Health{current: 20, max: 20},
+            Health{current: 10, max: 10},
         )
     );
 }
