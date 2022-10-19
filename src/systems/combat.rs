4e0254b0e6f56220eed45ff2fc10dda2239dffc7
petrostrak <pit.trak@gmail.com>
2022-10-19 18:38:29 +0300

    Catch error when player is dead

--- src/systems/combat.rs
+++ src/systems/combat.rs
@@ -3,6 +3,7 @@ use crate::prelude::*;
 #[system]
 #[read_component(WantsToAttack)]
 #[write_component(Health)]
+#[write_component(Player)]
 // This code iterates the victims collection you just created. Then it uses if 
 // let to activate only if the victim has health (once again, preventing you 
 // from mind- lessly beating up inanimate objects). It then reduces the 
@@ -18,17 +19,21 @@ pub fn combat(ecs: &mut SubWorld, commands: &mut CommandBuffer) {
     victims
         .iter()
         .for_each(|(message, victim)| {
+            let is_player = ecs
+                .entry_ref(*victim)
+                .unwrap()
+                .get_component::<Player>()
+                .is_ok();
+
             if let Ok(mut health) = ecs
                 .entry_mut(*victim)
                 .unwrap() 
                 .get_component_mut::<Health>()
             {
-                println!("Health before attack: {}", health.current); 
                 health.current -= 1;
-                if health.current < 1 {
+                if health.current < 1 && !is_player {
                     commands.remove(*victim); 
                 }
-                println!("Health after attack: {}", health.current); 
             }
             commands.remove(*message);
         });
