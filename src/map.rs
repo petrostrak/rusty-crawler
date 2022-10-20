a2b12caec87777b2773ed7e55d2b734ad717d7f0
petrostrak <pit.trak@gmail.com>
2022-10-20 12:31:34 +0300

    Revealing the map and update the map

--- src/map.rs
+++ src/map.rs
@@ -9,6 +9,7 @@ pub enum TileType {
 
 pub struct Map {
     pub tiles: Vec<TileType>,
+    pub revealed_tiles: Vec<bool>,
 }
 
 impl Algorithm2D for Map {
@@ -28,7 +29,8 @@ pub fn map_idx(x: i32, y: i32) -> usize {
 impl Map {
     pub fn new() -> Self {
         Self {
-            tiles: vec![TileType::Floor; NUM_TILES]
+            tiles: vec![TileType::Floor; NUM_TILES],
+            revealed_tiles: vec![false; NUM_TILES],
         }
     }
 
