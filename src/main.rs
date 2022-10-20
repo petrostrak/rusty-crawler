1762ccf1dacf8967654a2f4df84dde6f3720255a
petrostrak <pit.trak@gmail.com>
2022-10-20 10:31:12 +0300

    Refactor main

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 #![warn(clippy::pedantic)]
 
-mod map;
-mod map_builder;
-mod camera;
 mod components;
 mod spawner;
+mod map;
+mod map_builder;
 mod systems;
+mod camera;
 mod turn_state;
 
 mod prelude {
@@ -15,25 +15,25 @@ mod prelude {
     pub use legion::systems::CommandBuffer;
     pub const SCREEN_WIDTH: i32 = 80;
     pub const SCREEN_HEIGHT: i32 = 50;
-    pub const DISPLAY_WIDTH: i32 = SCREEN_WIDTH / 2; 
+    pub const DISPLAY_WIDTH: i32 = SCREEN_WIDTH / 2;
     pub const DISPLAY_HEIGHT: i32 = SCREEN_HEIGHT / 2;
-    pub use crate::map::*;
-    pub use crate::map_builder::*;
-    pub use crate::camera::*;
     pub use crate::components::*;
     pub use crate::spawner::*;
+    pub use crate::map::*;
     pub use crate::systems::*;
+    pub use crate::map_builder::*;
+    pub use crate::camera::*;
     pub use crate::turn_state::*;
 }
 
 use prelude::*;
 
 struct State {
-    ecs: World,
+    ecs : World,
     resources: Resources,
     input_systems: Schedule,
     player_systems: Schedule,
-    monster_systems: Schedule,
+    monster_systems: Schedule
 }
 
 impl State {
@@ -57,26 +57,7 @@ impl State {
             resources,
             input_systems: build_input_scheduler(),
             player_systems: build_player_scheduler(),
-            monster_systems: build_monster_scheduler(),
-        }
-    }
-
-    fn game_over(&mut self, ctx: &mut BTerm) {
-        ctx.set_active_console(2);
-        ctx.print_color_centered(2, RED, BLACK, "Your quest has ended.");
-        ctx.print_color_centered(4, WHITE, BLACK, 
-            "Slain by a monster, your hero's journey has come to a \
-            premature end.");
-        ctx.print_color_centered(5, WHITE, BLACK, 
-            "The Amulet of Yala remains unclaimed, and your home town \
-            is not saved.");
-        ctx.print_color_centered(8, YELLOW, BLACK, 
-            "Don't worry, you can always try again with a new hero.");
-        ctx.print_color_centered(9, GREEN, BLACK, 
-            "Press 1 to play again.");
-
-        if let Some(VirtualKeyCode::Key1) = ctx.key {
-            self.reset_game_state();
+            monster_systems: build_monster_scheduler()
         }
     }
 
@@ -97,12 +78,35 @@ impl State {
         self.resources.insert(TurnState::AwaitingInput);
     }
 
+    fn game_over(&mut self, ctx: &mut BTerm) {
+        ctx.set_active_console(2);
+        ctx.print_color_centered(2, RED, BLACK, "Your quest has ended.");
+        ctx.print_color_centered(4, WHITE, BLACK,
+        "Slain by a monster, your hero's journey has come to a \
+            premature end.");
+        ctx.print_color_centered(5, WHITE, BLACK,
+        "The Amulet of Yala remains unclaimed, and your home town \
+            is not saved.");
+        ctx.print_color_centered(8, YELLOW, BLACK,
+            "Don't worry, you can always try again with a new hero.");
+        ctx.print_color_centered(9, GREEN, BLACK, "Press 1 to play \
+            again.");
+
+        if let Some(VirtualKeyCode::Key1) = ctx.key {
+            self.reset_game_state();
+        }
+    }
+
     fn victory(&mut self, ctx: &mut BTerm) {
         ctx.set_active_console(2);
         ctx.print_color_centered(2, GREEN, BLACK, "You have won!");
-        ctx.print_color_centered(4, WHITE, BLACK, "You put on the Amulet of Yala and feel its power course through your veins.");
-        ctx.print_color_centered(5, WHITE, BLACK, "Your town is saved, and you can return to your normal life.");
-        ctx.print_color_centered(7, GREEN, BLACK, "Press 1 to play again.");
+        ctx.print_color_centered(4, WHITE, BLACK, 
+        "You put on the Amulet of Yala and feel its power course through \
+            your veins.");
+        ctx.print_color_centered(5, WHITE, BLACK, 
+            "Your town is saved, and you can return to your normal life.");
+        ctx.print_color_centered(7, GREEN, BLACK, "Press 1 to \
+            play again.");
         if let Some(VirtualKeyCode::Key1) = ctx.key {
             self.reset_game_state();
         }
@@ -110,57 +114,35 @@ impl State {
 }
 
 impl GameState for State {
-    fn tick(&mut self, ctx: &mut BTerm) { 
+    fn tick(&mut self, ctx: &mut BTerm) {
         ctx.set_active_console(0);
         ctx.cls();
         ctx.set_active_console(1);
         ctx.cls();
         ctx.set_active_console(2);
         ctx.cls();
-
-        // This makes the current keyboard state available 
-        // to any system that requests it.
         self.resources.insert(ctx.key);
-
-        // Identifying Monsters with Tooltips
         ctx.set_active_console(0);
-        self.resources.insert(Point::from_tuple(ctx.mouse_pos));
-
-        // self.resources.get::<TYPE> requests a resource of a given type (in this case TurnState) 
-        // from the ECS’s resources. The result is returned as an Option, so you need to unwrap() 
-        // it to access the contents. The final call to clone() duplicates the state. This ensures 
-        // that the resource is no longer borrowed—you’re looking at a copy of the current turn state, 
-        // rather than the original. This is another example of working around Rust’s borrow checker.
+        self.resources.insert(Point::from_tuple(ctx.mouse_pos()));
         let current_state = self.resources.get::<TurnState>().unwrap().clone();
         match current_state {
-            TurnState::AwaitingInput => self.input_systems.execute(
-                &mut self.ecs, 
-                &mut self.resources,
-            ),
+            TurnState::AwaitingInput => self.input_systems.execute(&mut self.ecs, &mut self.resources),
             TurnState::PlayerTurn => {
-                self.player_systems.execute(
-                    &mut self.ecs, 
-                    &mut self.resources
-                );
-            },
+                self.player_systems.execute(&mut self.ecs, &mut self.resources);
+            }
             TurnState::MonsterTurn => {
-                self.monster_systems.execute(
-                    &mut self.ecs, 
-                    &mut self.resources
-                );
-            },
-            TurnState::GameOver => {
-                self.game_over(ctx);
-            },
+                self.monster_systems.execute(&mut self.ecs, &mut self.resources)
+            }
+            TurnState::GameOver => self.game_over(ctx),
             TurnState::Victory => self.victory(ctx),
         }
-        render_draw_buffer(ctx).expect("render error")
+        render_draw_buffer(ctx).expect("Render error");
     }
 }
 
 fn main() -> BError {
     let context = BTermBuilder::new()
-        .with_title("Rusty Crawler")
+        .with_title("Dungeon Crawler")
         .with_fps_cap(30.0)
         .with_dimensions(DISPLAY_WIDTH, DISPLAY_HEIGHT)
         .with_tile_dimensions(32, 32)
@@ -169,7 +151,7 @@ fn main() -> BError {
         .with_font("terminal8x8.png", 8, 8)
         .with_simple_console(DISPLAY_WIDTH, DISPLAY_HEIGHT, "dungeonfont.png")
         .with_simple_console_no_bg(DISPLAY_WIDTH, DISPLAY_HEIGHT, "dungeonfont.png")
-        .with_simple_console_no_bg(DISPLAY_WIDTH*8, DISPLAY_HEIGHT*2, "terminal8x8.png")
+        .with_simple_console_no_bg(SCREEN_WIDTH*2, SCREEN_HEIGHT*2, "terminal8x8.png")
         .build()?;
 
     main_loop(context, State::new())
