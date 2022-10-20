3a2ec9ab40c5bd7905576a413acdc866871ad4bd
petrostrak <pit.trak@gmail.com>
2022-10-20 14:28:44 +0300

    Iterate away the chaos

--- src/map_builder/automata.rs
+++ src/map_builder/automata.rs
@@ -18,6 +18,36 @@ impl CellularAutomataArchitect {
             }
         });
     }
+
+    fn count_neighbors(&self, x: i32, y: i32, map: &Map) -> usize {
+        let mut neighbors = 0;
+        for iy in -1 ..= 1 {
+            for ix in -1 ..= 1 {
+
+                // Don't count the current tile, only count its neighbors.
+                if !(ix==0 && iy ==0) && map.tiles[map_idx(x+ix, y+iy)] == TileType::Wall {
+                    neighbors += 1;
+                }
+            }
+        }
+        neighbors
+    }
+
+    fn iteration(&mut self, map: &mut Map) {
+        let mut new_tiles = map.tiles.clone();
+        for y in 1 .. SCREEN_HEIGHT -1 {
+            for x in 1 .. SCREEN_WIDTH -1 {
+                let neighbors = self.count_neighbors(x, y, map);
+                let idx = map_idx(x, y);
+                if neighbors > 4 || neighbors == 0 {
+                    new_tiles[idx] = TileType::Wall;
+                } else {
+                    new_tiles[idx] = TileType::Floor;
+                }
+            }
+        }
+        map.tiles = new_tiles;
+    }
 }
 
 impl MapArchitect for CellularAutomataArchitect {
