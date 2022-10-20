acbbc8aae05f96e106e3dc759651c5e348a06b8f
petrostrak <pit.trak@gmail.com>
2022-10-20 15:09:56 +0300

    Carve caverns with drunken miner

--- src/map_builder/drunkard.rs
+++ src/map_builder/drunkard.rs
@@ -1,7 +1,46 @@
 use crate::prelude::*;
 use super::MapArchitect;
 
+// How far a miner can stumble before passing out.
+const STAGGER_DISTANE: usize = 400;
+
 pub struct DrunkardsWalkArchitect {}
+
+impl DrunkardsWalkArchitect {
+    fn drunkard(
+        &mut self,
+        start: &Point,
+        rng: &mut RandomNumberGenerator,
+        map: &mut Map
+    ) {
+        let mut drunkard_pos = start.clone();
+        let mut distance_staggered = 0;
+
+        loop {
+            let drunk_idx = map.point2d_to_index(drunkard_pos);
+            map.tiles[drunk_idx] = TileType::Floor;
+
+            // Randomly pick a direction and adjust the digger's current position
+            // accordingly.
+            match rng.range(0, 4) {
+                0 => drunkard_pos.x -= 1,
+                1 => drunkard_pos.x += 1,
+                2 => drunkard_pos.y -= 1,
+                _ => drunkard_pos.y += 1,
+            }
+
+            // If the digger has left the map, break.
+            if !map.in_bounds(drunkard_pos) {
+                break;
+            }
+
+            distance_staggered += 1;
+            if distance_staggered > STAGGER_DISTANE {
+                break;
+            }
+        }
+    }
+}
 
 impl MapArchitect for DrunkardsWalkArchitect {
     fn new(&mut self, rng: &mut RandomNumberGenerator) -> MapBuilder {
