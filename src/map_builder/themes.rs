4db0d964a56933f43342942198c153300546c44e
petrostrak <pit.trak@gmail.com>
2022-10-21 11:18:29 +0300

    Build a forest

--- src/map_builder/themes.rs
+++ src/map_builder/themes.rs
@@ -16,3 +16,20 @@ impl MapTheme for DungeonTheme {
         }
     }
 }
+
+pub struct ForestTheme {}
+
+impl ForestTheme {
+    pub fn new() -> Box<dyn MapTheme> {
+        Box::new(Self{})
+    }
+}
+
+impl MapTheme for ForestTheme {
+    fn tile_to_render(&self, tile_type: TileType) -> FontCharType {
+        match tile_type {
+            TileType::Floor => to_cp437(';'),
+            TileType::Wall => to_cp437('"'),
+        }
+    }
+}
