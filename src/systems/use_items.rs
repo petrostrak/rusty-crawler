9821206c91de166b2af6fbf41358834836e3153f
petrostrak <pit.trak@gmail.com>
2022-10-21 15:20:55 +0300

    Iterate item application

--- src/systems/use_items.rs
+++ src/systems/use_items.rs
@@ -6,9 +6,26 @@ use crate::prelude::*;
 #[write_component(Health)]
 #[read_component(ProvidesDungeonMap)]
 pub fn use_items(
-    ecs: &mut SubWorld,
+    ecs: &mut SubWorld, 
     commands: &mut CommandBuffer,
-    #[resource] map: &mut Map,
+    #[resource] map: &mut Map
 ) {
     let mut healing_to_apply = Vec::<(Entity, i32)>::new();
+    <(Entity, &ActivateItem)>::query().iter(ecs)
+    .for_each(|(entity, activate)| {
+
+        let item = ecs.entry_ref(activate.item);
+        if let Ok(item) = item {
+            if let Ok(healing) = item.get_component::<ProvidesHealing>() {
+                healing_to_apply.push((activate.used_by, healing.amount));
+            }
+
+            if let Ok(_mapper) = item.get_component::<ProvidesDungeonMap>() {
+                map.revealed_tiles.iter_mut().for_each(|t| *t = true);
+            }
+        }
+
+        commands.remove(activate.item);
+        commands.remove(*entity);
+    });
 }
