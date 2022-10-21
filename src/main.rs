0f71137092e0104e93a4bcb187b7bdb2a60d3820
petrostrak <pit.trak@gmail.com>
2022-10-21 11:30:05 +0300

    Use the selected theme

--- src/main.rs
+++ src/main.rs
@@ -50,6 +50,7 @@ impl State {
         resources.insert(map_builder.map);
         resources.insert(Camera::new(map_builder.player_start));
         resources.insert(TurnState::AwaitingInput);
+        resources.insert(map_builder.theme);
         Self {
             ecs,
             resources,
@@ -72,21 +73,19 @@ impl State {
         self.resources.insert(map_builder.map);
         self.resources.insert(Camera::new(map_builder.player_start));
         self.resources.insert(TurnState::AwaitingInput);
+        self.resources.insert(map_builder.theme);
     }
 
     fn game_over(&mut self, ctx: &mut BTerm) {
         ctx.set_active_console(2);
         ctx.print_color_centered(2, RED, BLACK, "Your quest has ended.");
-        ctx.print_color_centered(4, WHITE, BLACK,
-        "Slain by a monster, your hero's journey has come to a \
-            premature end.");
-        ctx.print_color_centered(5, WHITE, BLACK,
-        "The Amulet of Yala remains unclaimed, and your home town \
-            is not saved.");
-        ctx.print_color_centered(8, YELLOW, BLACK,
+        ctx.print_color_centered(4, WHITE, BLACK, 
+            "Slain by a monster, your hero's journey has come to a premature end.");
+        ctx.print_color_centered(5, WHITE, BLACK, 
+            "The Amulet of Yala remains unclaimed, and your home town is not saved.");
+        ctx.print_color_centered(8, YELLOW, BLACK, 
             "Don't worry, you can always try again with a new hero.");
-        ctx.print_color_centered(9, GREEN, BLACK, "Press 1 to play \
-            again.");
+        ctx.print_color_centered(9, GREEN, BLACK, "Press 1 to play again.");
 
         if let Some(VirtualKeyCode::Key1) = ctx.key {
             self.reset_game_state();
@@ -97,12 +96,10 @@ impl State {
         ctx.set_active_console(2);
         ctx.print_color_centered(2, GREEN, BLACK, "You have won!");
         ctx.print_color_centered(4, WHITE, BLACK, 
-        "You put on the Amulet of Yala and feel its power course through \
-            your veins.");
+            "You put on the Amulet of Yala and feel its power course through your veins.");
         ctx.print_color_centered(5, WHITE, BLACK, 
             "Your town is saved, and you can return to your normal life.");
-        ctx.print_color_centered(7, GREEN, BLACK, "Press 1 to \
-            play again.");
+        ctx.print_color_centered(7, GREEN, BLACK, "Press 1 to play again.");
         if let Some(VirtualKeyCode::Key1) = ctx.key {
             self.reset_game_state();
         }
@@ -129,8 +126,12 @@ impl GameState for State {
             TurnState::MonsterTurn => {
                 self.monster_systems.execute(&mut self.ecs, &mut self.resources)
             }
-            TurnState::GameOver => self.game_over(ctx),
-            TurnState::Victory => self.victory(ctx),
+            TurnState::GameOver => {
+                self.game_over(ctx);
+            }
+            TurnState::Victory => {
+                self.victory(ctx);
+            }
         }
         render_draw_buffer(ctx).expect("Render error");
     }
