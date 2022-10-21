bfafeec0c4e0fc35d3a5a9f1872c587487b9cd95
petrostrak <pit.trak@gmail.com>
2022-10-21 15:12:11 +0300

    Sending an item activation message

--- src/components.rs
+++ src/components.rs
@@ -73,3 +73,9 @@ pub struct ProvidesDungeonMap;
 
 #[derive(Clone, PartialEq)]
 pub struct Carried(pub Entity);
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub struct ActivateItem {
+    pub used_by: Entity,
+    pub item: Entity,
+}
--- src/systems/player_input.rs
+++ src/systems/player_input.rs
@@ -1,3 +1,5 @@
+use std::collections::btree_map::Iter;
+
 use crate::prelude::*;
 
 // The #[system] line annotates the player_input 
@@ -46,6 +48,15 @@ pub fn player_input(
                 );
                 Point::new(0, 0)
             },
+            VirtualKeyCode::Key1 => use_item(0, ecs, commands),
+            VirtualKeyCode::Key2 => use_item(1, ecs, commands),
+            VirtualKeyCode::Key3 => use_item(2, ecs, commands),
+            VirtualKeyCode::Key4 => use_item(3, ecs, commands),
+            VirtualKeyCode::Key5 => use_item(4, ecs, commands),
+            VirtualKeyCode::Key6 => use_item(5, ecs, commands),
+            VirtualKeyCode::Key7 => use_item(6, ecs, commands),
+            VirtualKeyCode::Key8 => use_item(7, ecs, commands),
+            VirtualKeyCode::Key9 => use_item(8, ecs, commands),
             _ => Point::new(0, 0),
         };
 
@@ -95,4 +106,26 @@ pub fn player_input(
         }
         *turn_state = TurnState::PlayerTurn;
     }
+}
+
+fn use_item(n: usize, ecs: &mut SubWorld, commands: &mut CommandBuffer) -> Point {
+    let player_entity = <(Entity, &Player)>::query()
+        .iter(ecs)
+        .find_map(|(entity, _player)| Some(*entity))
+        .unwrap();
+    
+    let item_entity = <(Entity, &Item, &Carried)>::query()
+        .iter(ecs)
+        .filter(|(_, _, carried)| carried.0 == player_entity)
+        .enumerate()
+        .filter(|(item_count, (_, _, _))| *item_count == n)
+        .find_map(|(_, (item_entity, _, _))| Some(*item_entity));
+    if let Some(item_entity) = item_entity {
+        commands
+            .push(((), ActivateItem{
+                used_by: player_entity,
+                item: item_entity,
+            }));
+    }
+    Point::zero()
 }
