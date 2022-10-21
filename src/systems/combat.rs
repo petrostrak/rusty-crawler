4af8e65229e1cc6fd32c9c16a2cad384330c2d60
petrostrak <pit.trak@gmail.com>
2022-10-21 22:19:39 +0300

    Doing Some Damage

--- src/systems/combat.rs
+++ src/systems/combat.rs
@@ -1,9 +1,11 @@
 use crate::prelude::*;
 
-#[system]
-#[read_component(WantsToAttack)]
+#[system] 
+#[read_component(WantsToAttack)] 
+#[read_component(Player)] 
 #[write_component(Health)]
-#[write_component(Player)]
+#[read_component(Damage)]
+#[read_component(Carried)]
 // This code iterates the victims collection you just created. Then it uses if 
 // let to activate only if the victim has health (once again, preventing you 
 // from mind- lessly beating up inanimate objects). It then reduces the 
@@ -11,30 +13,45 @@ use crate::prelude::*;
 // one, the victim is deleted from the game. Finally, it deletes the WantsToAttack message.
 pub fn combat(ecs: &mut SubWorld, commands: &mut CommandBuffer) {
     let mut attackers = <(Entity, &WantsToAttack)>::query();
-    let victims : Vec<(Entity, Entity)> = attackers
+    let victims : Vec<(Entity, Entity, Entity)> = attackers
         .iter(ecs)
-        .map(|(entity, attack)| (*entity, attack.victim))
+        .map(|(entity, attack)| (*entity, attack.attacker, attack.victim) )
         .collect();
-    
-    victims
-        .iter()
-        .for_each(|(message, victim)| {
-            let is_player = ecs
-                .entry_ref(*victim)
-                .unwrap()
-                .get_component::<Player>()
-                .is_ok();
 
-            if let Ok(mut health) = ecs
-                .entry_mut(*victim)
-                .unwrap() 
-                .get_component_mut::<Health>()
-            {
-                health.current -= 1;
-                if health.current < 1 && !is_player {
-                    commands.remove(*victim); 
-                }
+    victims.iter().for_each(|(message, attacker, victim)| {
+        let is_player = ecs
+            .entry_ref(*victim)
+            .unwrap()
+            .get_component::<Player>()
+            .is_ok();
+
+        let base_damage = if let Ok(v) = ecs.entry_ref(*attacker) {
+            if let Ok(dmg) = v.get_component::<Damage>() {
+                dmg.0
+            } else {
+                0
             }
-            commands.remove(*message);
-        });
+        } else {
+            0
+        };
+
+        let weapon_damage : i32 = <(&Carried, &Damage)>::query().iter(ecs)
+            .filter(|(carried, _)| carried.0 == *attacker)
+            .map(|(_, dmg)| dmg.0)
+            .sum();
+
+        let final_damage = base_damage + weapon_damage;
+
+        if let Ok(mut health) = ecs
+            .entry_mut(*victim)
+            .unwrap()
+            .get_component_mut::<Health>()
+        {
+            health.current -= final_damage;
+            if health.current < 1 && !is_player {
+                commands.remove(*victim);
+            }
+        }
+        commands.remove(*message);
+    });
 }
