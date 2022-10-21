07623b14510b41b187faf5dcbe54cd63fb1c8def
petrostrak <pit.trak@gmail.com>
2022-10-21 21:41:16 +0300

    Data-Driven Spawning

--- src/spawner/template.rs
+++ src/spawner/template.rs
@@ -28,16 +28,77 @@ pub struct Templates {
 
 impl Templates {
     pub fn load() -> Self {
-
-        // File::open returns a Result. Since the file may not exist, or you
-        // may not have access to it, you need to handle the error.
         let file = File::open("resources/template.ron")
             .expect("Failed opening file");
-        
-        // from_reader calls Serde to open the file. If Serde cannot read the contents
-        // of the file, the function will return an error, so we use expect to unwrap 
-        // the returned option.
-        from_reader(file)
-            .expect("Unable to load templates")
+        from_reader(file).expect("Unable to load templates")
+    }
+
+    pub fn spawn_entities(
+        &self,
+        ecs: &mut World,
+        rng: &mut RandomNumberGenerator,
+        level: usize,
+        spawn_points: &[Point]
+    ) {
+        let mut available_entities = Vec::new();
+        self.entities
+            .iter()
+            .filter(|e| e.levels.contains(&level))
+            .for_each(|t| {
+                for _ in 0 .. t.frequency {
+                    available_entities.push(t);
+                }
+            }
+        );
+
+        let mut commands = CommandBuffer::new(ecs);
+        spawn_points.iter().for_each(|pt| {
+            if let Some(entity) = rng.random_slice_entry(&available_entities) {
+                self.spawn_entity(pt, entity, &mut commands);
+            }
+        });
+        commands.flush(ecs);
+    }
+
+    fn spawn_entity(
+        &self,
+        pt: &Point,
+        template: &Template,
+        commands: &mut CommandBuffer
+    ) {
+        let entity = commands.push((
+            pt.clone(),
+            Render{
+                color: ColorPair::new(WHITE, BLACK),
+                glyph: to_cp437(template.glyph)
+            },
+            Name(template.name.clone())
+        ));
+        match template.entity_type {
+            EntityType::Item => commands.add_component(entity, Item{}),
+            EntityType::Enemy => {
+                commands.add_component(entity, Enemy{});
+                commands.add_component(entity, FieldOfView::new(6));
+                commands.add_component(entity, ChasingPlayer{});
+                commands.add_component(entity, Health{
+                    current: template.hp.unwrap(),
+                    max: template.hp.unwrap()
+                });
+            }
+        }
+        if let Some(effects) = &template.provides {
+            effects.iter().for_each(|(provides, n)| {
+                match provides.as_str() {
+                    "Healing" => commands.add_component(entity,
+                        ProvidesHealing{ amount: *n}),
+                    "MagicMap" => commands.add_component(entity,
+                        ProvidesDungeonMap{}),
+                    _ => {
+                        println!("Warning: we don't know how to provide {}"
+                            , provides);
+                    }
+                }
+            });
+        }
     }
 }
