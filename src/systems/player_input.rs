573593b3ea1d3e9f89727e34b188ee9a707b5563
petrostrak <pit.trak@gmail.com>
2022-10-21 15:27:41 +0300

    Remove waiting to heal

--- src/systems/player_input.rs
+++ src/systems/player_input.rs
@@ -93,16 +93,6 @@ pub fn player_input(
                         destination
                     }));
             }
-
-            if !did_something {
-                if let Ok(mut health) = ecs
-                    .entry_mut(player_entity)
-                    .unwrap()
-                    .get_component_mut::<Health>()
-                {
-                    health.current = i32::min(health.max, health.current+1);
-                }
-            }
         }
         *turn_state = TurnState::PlayerTurn;
     }
